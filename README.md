22fd705e497fa2a10cedbf2ae52dd3f641d98c10
STCProject2022 <asifoffical429@gmail.com>
2022-09-12 17:06:53 +0500

    MVVM Architecture (final)

---
+++ README.md
@@ -0,0 +1,7 @@
+# Coin Paprika API:
+
+# Get coins route:
+https://api.coinpaprika.com/v1/coins
+
+# Get a coin by ID route:
+https://api.coinpaprika.com/v1/coins/btc-bitcoin
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
@@ -0,0 +1,42 @@
+package com.plcoding.cryptocurrencyappyt.di
+
+import com.plcoding.cryptocurrencyappyt.common.Constants
+import com.plcoding.cryptocurrencyappyt.data.remote.CoinPaprikaApi
+import com.plcoding.cryptocurrencyappyt.data.repository.CoinRepositoryImpl
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providePaprikaApi(): CoinPaprikaApi {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(api: CoinPaprikaApi): CoinRepository {
+        return CoinRepositoryImpl(api)
+    }
+}
